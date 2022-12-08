49bd3e45ac5331580868870eeec28444bb6bff25
Yamakasuki <gabriel.yamamoto123@hotmail.com>
2022-12-07 23:55:56 -0300

    gatinho

--- main.asm
+++ main.asm
@@ -10,6 +10,7 @@
             db ' ','6',4 dup(' '),'2','8',' '
             db 3 dup(' '),'4','1','9',2 dup(' '),'5'
             db 4 dup(' '),'8',2 dup(' '),'7','9'
+            
 
     vetor_val dw 60 dup (' ')
 
@@ -22,6 +23,8 @@
     clique2_msg db 'na tabela, clique$'
     clique3_msg db 'no espaco e digite$'
     clique4_msg db 'um valor entre 1 a 9$'
+    Hexa db "HEXA<3$"
+    FraseVitoria db "PARABENS VOCE TROUXE O HEXA PARA O BRASIL$"    
 
     flag db '?'
 .code
@@ -213,14 +216,15 @@
 
         FIM:            ;fim do programa
 
+
         mov ax, 3        ;clear screen
         int 10h
 
         XOR DX,DX
         PUSH DX
         
-        LEA SI,matriz
-        CALL imprimematriz
+        
+        CALL BANDEIRA
         
         MOV AH,4CH
         int 21h
@@ -608,6 +612,208 @@ FIMCOLOCAVALOR:
         reg_pop
         ret 
     valores_imutaveis endp
+    
+    BANDEIRA proc
+    reg_push
+
+    ;640x200
+        MOV ah,00h
+        MOV al,0eh
+        int 10h
+
+
+        MOV AH,02
+        MOV DH,6              ;Posicionando cursor no meio superior (linha 1,coluna 32)
+        MOV DL,17
+        MOV BH,0
+        int 10h
+
+        mov ah,09h
+        lea dx,FraseVitoria
+        int 21h
+
+    ;VERDE PROC
+
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,160
+        mov dx,103
+        mov al,2
+
+
+
+    linhavertical1:
+        int 10h
+        inc dx
+        cmp dx,181
+        jne linhavertical1
+
+        
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,160
+        mov dx,103
+        mov al,2
+    linhahorizontal1:
+        int 10h
+        inc cx
+        cmp cx,467
+        jne linhahorizontal1
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,160
+        mov dx,180
+        mov al,2
+    linhahorizontal2:
+        int 10h
+        inc cx
+        cmp cx,467
+        jne linhahorizontal2
+
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,467
+        mov dx,103
+        mov al,2
+    linhavertical2:
+        int 10h
+        inc dx
+        cmp dx,181
+        jne linhavertical2
+    ;VERDE ENDP
+
+
+    ;AMARELO PROC
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,160
+        mov dx,141
+        mov al,14
+    linha:
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        
+        
+        
+        int 10h
+        inc dx
+        
+        cmp cx,312
+        jne linha
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,312
+        mov dx,103
+        mov al,14
+    linha4:
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        
+        
+        
+        int 10h
+        inc dx
+        
+        cmp dx,142
+        jne linha4
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,312
+        mov dx,179
+        mov al,14
+    linha2:
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        
+        int 10h
+        dec dx
+        
+        cmp dx,140
+        jne linha2
+
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,160
+        mov dx,141
+        mov al,14
+    linha3:
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        int 10h
+        inc cx
+        
+        int 10h
+        dec dx
+        
+        cmp dx,103
+        jne linha3
+    ;AMARELO ENDPROC
+
+    ;AZUL PROC
+    ; draw pixel -coluna -linha  -cor
+
+        mov ah,0CH
+        mov cx,260
+        mov dx,123
+        mov al,1
+
+
+    pinta:
+    quadrado:
+        int 10h
+        inc dx
+        cmp dx,160
+        jne quadrado
+
+        mov dx,123
+        inc cx
+        cmp cx,365
+        jne pinta
+    ;AZUL ENDPROC
+
+    MOV AH,02
+    MOV DH,17              ;Posicionando cursor no meio superior (linha 1,coluna 32)
+    MOV DL,37
+    MOV BH,0
+    int 10h
+
+    mov ah,09h
+    lea dx,Hexa
+    int 21h
+
+    reg_pop
+    ret
+    BANDEIRA endp
 
     END MAIN
 
